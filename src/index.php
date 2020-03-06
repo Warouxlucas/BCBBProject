716c2ba33eb6c7794d4ef89f32a9880390159045
Warouxlucas <Warouxlucas@hotmail.com>
2020-03-06 09:58:58 +0100

    formulaire de connection, essais de sessions, bouton deroulant boostrap, remember me (premiere util de cookies)

--- src/index.php
+++ src/index.php
@@ -54,45 +54,69 @@ $stmp= null;
 
 
 var_dump($topics[0]);
-        ?>
-        <!DOCTYPE html>
-<html>
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<title>Cours Complet Bootstrap 4</title>
-<meta charset='utf-8'>
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <title>I hate bootstrap</title>
+        <meta charset='utf-8'>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+                crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+                integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+                crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+                integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+                crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+                integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+                crossorigin="anonymous"></script>
 </head>
+
 <body>
-<div class="container">
-        <h1>Navigation</h1>
-        <a href="login.php">login</a>
-                <div class="row">
-        <?php 
+        <main>
+                <!-- boucle on boards to show them all -->
+                <?php 
+        echo "<ul class='boards_list_container'>";
                 foreach($boards as $board){
                         $boardsname= $board->name;
-        echo '<div class="dropdown">'.
-        '<button class="btn btn-secondary dropdown-toggle" type="button" id="'.$boardsname.'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'.$boardsname.'</button>'.
-        '<div class="dropdown-menu" aria-labelledby="'.$boardsname.'">';
-        $i=0;
+                        echo 
+                                "<li><div class='boards_list_title'><a href='#'>".                                
+                                $boardsname. 
+                                "</a></div>". 
+                                "<div class='boards_list_author'>". 
+                                "</div>".
+                                "<ul>";
+                                $i=0;
                                         foreach($topics as $topic){
                                         $topicname=$topic->name;
                                         // var_dump($boardsname);
                                         
                                                 if($boardsname == $topicname && $i<3){
                                                         $i++;
-                                                echo '<a class="dropdown-item" href="#">'.$topic->title.'</a>';
-                                                        ;
+                                                echo  "<li><div class='topics_list_title'>".                         
+                                $topic->title.
+                                "</div>". 
+                                "<div class='topics_list_author'>". 
+                                $topic->nickname.
+                                "</div>".
+                                "</li>";}
+                        }
+                                        
+                                echo "</ul>".
+                                "</li>";
+                                
+                };
+                echo "</ul>";
+        ?>
 
-                                                }
-                                                
-                                        }
-                                        echo '<a class="dropdown-item" href="#">See more...</a>'.'</div></div>';
-                                }
-?>
-        </div>
-        </div>
-        
+
+        </main>
+
+</body>
+
+</html>
