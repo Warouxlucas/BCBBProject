6bf375dd66664e20462571c5b5357cf7c2f0f8ab
Warouxlucas <Warouxlucas@hotmail.com>
2020-03-09 14:55:15 +0100

    rename index

--- src/home.php
+++ src/home.php
@@ -60,7 +60,7 @@ var_dump($topics[0]);
 <html lang="en">
 
 <head>
-        <title>I hate bootstrap</title>
+        <title>home</title>
         <meta charset='utf-8'>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
