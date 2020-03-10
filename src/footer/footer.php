27547fc58822a3fb1b865129074da7b9a8dd63b2
Mustafa <m.devbe@gmail.com>
2020-03-10 11:55:57 +0100

    upload footer

---
+++ src/footer/footer.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer-style/footer.css">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <footer>
+        <hr>
+        <div class="footer">
+            <h1>The Weaver</h1>
+
+            <ul>
+                <li><a href="">HOME</a></li>
+                <li><a href="">FAQ</a></li>
+                <li><a href="">CONTACT</a></li>
+                <li><a href="">ABOUT US</a></li>
+            </ul>
+
+
+            <div class="icon">
+                <ul>
+                    <li><a href="https://www.facebook.com" target="_blank"><img src="icon/facebook.png" alt="" height="17em"></a></li>
+                    <li><a href="https://www.twitter.com/" target="_blank"><img src="icon/twitter.png" alt="" height="17em"></a></li>
+                    <li><a href="https://www.git.com/" target="_blank"><img src="icon/git.png" alt="" height="17em"></a></li>
+                    <li><a href="https://www.youtube.com/" target="_blank"><img src="icon/youtube.png" alt="" height="17em"></a></li>
+                </ul>
+            </div>
+        </div>
+        <p> &copy; <small>The Weaver 2020 Liége </small> </p>
+    </footer>
+        
+    
+</body>
+</html>
