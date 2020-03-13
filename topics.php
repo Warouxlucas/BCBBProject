5cb7a799da7169b268ffe8fd7a0fd0dbc5fcbfda
Warouxlucas <Warouxlucas@hotmail.com>
2020-03-13 17:00:32 +0100

    connection bd fixed 2

--- topics.php
+++ topics.php
@@ -33,7 +33,8 @@ include 'nav.php';
 
 
     try {
-        $bdd = new PDO('mysql:host=mysql;dbname=BCBB;charset=utf8', 'root', 'root');
+        $db = new PDO('mysql:host=g4yltwdo6z0izlm6.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=svb7vo33swlkw6jp;charset=utf8mb4',
+        'iicuafj3oduynv19','uqdhhz7xw60z5p06');
     } catch (Exception $err) {
         die('Erreur : ' . $err->getMessage());
     }
