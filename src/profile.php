e5a8548a90280e33ae2c767c43f8ab31697db351
Warouxlucas <Warouxlucas@hotmail.com>
2020-03-18 15:57:36 +0100

    no update

--- src/profile.php
+++ src/profile.php
@@ -1,4 +1,4 @@
-<?php session_start()?> 
+<?php session_start()?>
 
 <?php
 include("connect/connect.php");    
@@ -16,54 +16,63 @@ $stmp= null;
 ?>
 
 <!DOCTYPE html>
-<html>    
-<head>        
- <meta charset="UTF-8">
- <link rel="stylesheet" href="style/profile.css">
- <link rel="stylesheet" href="footer/footer-style/footer.css">
-         <title><?php echo $user[0]->firstname;echo " "; echo $user[0]->lastname; ?>'s Profile</title>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style/profile.css">
+    <link rel="stylesheet" href="footer/footer-style/footer.css">
+    <title><?php echo $user[0]->firstname;echo " "; echo $user[0]->lastname; ?>'s Profile</title>
 </head>
-    <body class="body">
-        <div class="profile">
-            <div class="j-profile">
-                <?php include 'navbar/nav.php'?>
 
-                <!-- <a href="index.php">Home</a> | <?php //echo $user[0]->firstname;echo " "; echo $user[0]->lastname; ?>'s Profile         -->
-                <h3>Personal Information  <?php  //  $visitor = $_SESSION['username'];
+<body class="body">
+    <div class="profile">
+        <div class="j-profile">
+            <?php include 'navbar/nav.php'?>
+
+            <!-- <a href="index.php">Home</a> | <?php //echo $user[0]->firstname;echo " "; echo $user[0]->lastname; ?>'s Profile         -->
+            <h3>Personal Information <?php  //  $visitor = $_SESSION['username'];
                     // if ($user == $visitor)
-            { ?>                       <?php
+            { ?> <?php
             }
-                    ?>        </h3>
-                    <img src="https://www.gravatar.com/avatar/<?php echo md5( strtolower( trim( $user[0]->email ) ) ) ?>" />       
-                    <table>
-                                
-                                <tr>
-                                    <td>Email:</td><td><?php echo $user[0]->email?></td> 
-                                </tr>
-                                <tr>                
-                                <td>Full name:</td><td><?php echo $user[0]->firstname;echo " "; echo $user[0]->lastname; ?></td>   
-                                </tr>
-                                <tr>
-                                <td>Birthdate:</td><td><?php echo $user[0]->birthday?></td>  <!-- need to change the date format to that dd-mm-yyyy-->
-                                </tr>
-                                <tr>                
-                                <td>Nickname:</td><td><?php echo $user[0]->nickname?></td> 
-                                </tr> 
-                                <tr>
-                                    <td>Gender:</td><td><?php if($user[0]->gender == 1)echo 'male'; else echo 'female' ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Signature:</td><td><?php echo $user[0]->signature?></td> 
-                                </tr></br>  
-                                    
-                    </table> 
-                    <button onclick="location.href='http://localhost/profile2.php'" type="button">
-                    Edit your profile</button>
-                </div>
+                    ?> </h3>
+            <img src="https://www.gravatar.com/avatar/<?php echo md5( strtolower( trim( $user[0]->email ) ) ) ?>" />
+            <table>
 
-            
-            
+                <tr>
+                    <td>Email:</td>
+                    <td><?php echo $user[0]->email?></td>
+                </tr>
+                <tr>
+                    <td>Full name:</td>
+                    <td><?php echo $user[0]->firstname;echo " "; echo $user[0]->lastname; ?></td>
+                </tr>
+                <tr>
+                    <td>Birthdate:</td>
+                    <td><?php echo $user[0]->birthday?></td> <!-- need to change the date format to that dd-mm-yyyy-->
+                </tr>
+                <tr>
+                    <td>Nickname:</td>
+                    <td><?php echo $user[0]->nickname?></td>
+                </tr>
+                <tr>
+                    <td>Gender:</td>
+                    <td><?php if($user[0]->gender == 1)echo 'male'; else echo 'female' ?></td>
+                </tr>
+                <tr>
+                    <td>Signature:</td>
+                    <td><?php echo $user[0]->signature?></td>
+                </tr></br>
+
+            </table>
+            <button onclick="location.href='http://localhost/profile2.php'" type="button">
+                Edit your profile</button>
         </div>
-        <?php include('footer/footer.php') ?>
-    </body>
-</html> 
+
+
+
+    </div>
+    <?php include('footer/footer.php') ?>
+</body>
+
+</html>
